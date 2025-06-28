cfc5032f483e18f0b840ccd46ca44f2882e18478
Djuanzz <adnanjuan06@gmail.com>
2025-06-29 02:38:36 +0700

    feat: process

--- server_process_pool_http.py
+++ server_process_pool_http.py
@@ -1,73 +1,44 @@
-from socket import *
-import socket
-import time
-import sys
-import logging
-import multiprocessing
+from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 from concurrent.futures import ProcessPoolExecutor
 from http import HttpServer
 
 httpserver = HttpServer()
 
-#untuk menggunakan processpoolexecutor, karena tidak mendukung subclassing pada process,
-#maka class ProcessTheClient dirubah dulu menjadi function, tanpda memodifikasi behaviour didalamnya
-
-def ProcessTheClient(connection,address):
-		rcv=""
-		while True:
-			try:
-				data = connection.recv(32)
-				if data:
-					#merubah input dari socket (berupa bytes) ke dalam string
-					#agar bisa mendeteksi \r\n
-					d = data.decode()
-					rcv=rcv+d
-					if rcv[-2:]=='\r\n':
-						#end of command, proses string
-						#logging.warning("data dari client: {}" . format(rcv))
-						hasil = httpserver.proses(rcv)
-						#hasil akan berupa bytes
-						#untuk bisa ditambahi dengan string, maka string harus di encode
-						hasil=hasil+"\r\n\r\n".encode()
-						#logging.warning("balas ke  client: {}" . format(hasil))
-						#hasil sudah dalam bentuk bytes
-						connection.sendall(hasil)
-						rcv=""
-						connection.close()
-						return
-				else:
-					break
-			except OSError as e:
-				pass
-		connection.close()
-		return
-
-
+def ProcessTheClient(connection, address):
+    rcv = ""
+    print(f"[DEBUG] Connected from {address}")
+    while True:
+        try:
+            data = connection.recv(1024)
+            if data:
+                d = data.decode()
+                rcv += d
+                if "\r\n\r\n" in rcv:
+                    print(f"[DEBUG] Request from {address}:\n{rcv}\n---")
+                    hasil = httpserver.proses(rcv)
+                    connection.sendall(hasil)
+                    print(f"[DEBUG] Response sent to {address}")
+                    rcv = ""
+                    break
+            else:
+                break
+        except OSError as e:
+            print(f"[ERROR] Socket error: {e}")
+            break
+    connection.close()
+    print(f"[DEBUG] Connection closed for {address}")
 
 def Server():
-	the_clients = []
-	my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket = socket(AF_INET, SOCK_STREAM)
+    my_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    my_socket.bind(('0.0.0.0', 8889))
+    my_socket.listen(5)
+    print("ProcessPool Server listening on port 8889")
 
-	my_socket.bind(('0.0.0.0', 8889))
-	my_socket.listen(1)
+    with ProcessPoolExecutor(20) as executor:
+        while True:
+            connection, address = my_socket.accept()
+            executor.submit(ProcessTheClient, connection, address)
 
-	with ProcessPoolExecutor(20) as executor:
-		while True:
-				connection, client_address = my_socket.accept()
-				#logging.warning("connection from {}".format(client_address))
-				p = executor.submit(ProcessTheClient, connection, client_address)
-				the_clients.append(p)
-				#menampilkan jumlah process yang sedang aktif
-				jumlah = ['x' for i in the_clients if i.running()==True]
-				print(jumlah)
-
-
-
-
-
-def main():
-	Server()
-
-if __name__=="__main__":
-	main()
+if __name__ == "__main__":
+    Server()
